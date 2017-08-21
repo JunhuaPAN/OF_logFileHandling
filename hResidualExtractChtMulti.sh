18c18029fc376a6418e7a28775ce8c10c35a42bc
snaka-dev <snaka-dev@users.noreply.github.com>
2017-08-21 18:21:14 +0900

    sample script for extracting h-residuals for chtMul
    sample script for extracting h-residuals for chtMultigionSimpleFoam tutorial "multiRegionHeaterRadiation".

---
+++ hResidualExtractChtMulti.sh
@@ -0,0 +1,11 @@
+cat log.chtMultiRegionSimpleFoam | grep -v 'ExecutionTime' | grep 'Time = '|tr -d 'Time = ' > log.time
+
+cat log.chtMultiRegionSimpleFoam |grep 'Solving for h'|sed '1~5!d'  |cut -d ' ' -f9 |tr -d ',' > log.bottomAir-hResidual
+cat log.chtMultiRegionSimpleFoam |grep 'Solving for h'|sed '2~5!d'  |cut -d ' ' -f9 |tr -d ',' > log.topAir-hResidual
+cat log.chtMultiRegionSimpleFoam |grep 'Solving for h'|sed '3~5!d'  |cut -d ' ' -f9 |tr -d ',' > log.heater-hResidual
+cat log.chtMultiRegionSimpleFoam |grep 'Solving for h'|sed '4~5!d'  |cut -d ' ' -f9 |tr -d ',' > log.leftSolid-hResidual
+cat log.chtMultiRegionSimpleFoam |grep 'Solving for h'|sed '5~5!d'  |cut -d ' ' -f9 |tr -d ',' > log.rightSolid-hResidual
+
+paste log.time  log.bottomAir-hResidual log.topAir-hResidual log.heater-hResidual log.leftSolid-hResidual log.rightSolid-hResidual > log.hResidual
+sed '1i\# Time bottomAir topAir heater leftSolid rightSolid' -i log.hResidual
+sed '1i\# hResidual' -i log.hResidual
